f8d13b2a412130151735596c647606ed5eca7d17
Stefan Grandner <stefan.grandner@stayfriends.com>
2022-02-04 03:39:31 +0100

    implemented Koch snowflake algorithm

--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -5,16 +5,51 @@ const Canvas = props => {
 
     const canvasRef = useRef(null);
 
-    const draw = ctx => {
+    const clearCanvas = (ctx) => {
+
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    };
+
+    const drawText = (ctx, text, x, y) => {
+
+        ctx.font = '20px serif';
+        ctx.fillStyle = '#000000';
+        ctx.fillText(text, x, y);
+    };
+
+    const drawLineInit = (ctx, x, y) => {
 
         ctx.beginPath();
-        ctx.moveTo(10, 10);
-        ctx.lineTo(200, 200);
-        ctx.stroke();
+        ctx.moveTo(x, y);
+    };
 
-        ctx.beginPath();
-        ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    const drawLine = (ctx, x, y) => {
+
+        ctx.lineTo(x, y);
         ctx.stroke();
+    };
+
+    const ruleStep = [ 'L', 'R', 'L' ];
+    const recursionSteps = 3;
+
+    const determineRule = (step, index) => {
+
+        if (index > recursionSteps) {
+            return [];
+        }
+
+        const nextStepPart = determineRule(ruleStep, ++index);
+
+        let nextStep = [];
+        step.forEach((element) => {
+
+            nextStep.push(...nextStepPart);
+            nextStep.push(element);
+        });
+        nextStep.push(...nextStepPart);
+
+        return nextStep;
     };
 
     useEffect(() => {
@@ -22,7 +57,43 @@ const Canvas = props => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
 
-        draw(context);
+        clearCanvas(context);
+
+        if (recursionSteps > 5) {
+            drawText(context, 'too many recursion steps (max. 5) !', 30, 50);
+            return;
+        }
+
+        const rule = determineRule([ 'R', 'R' ], 0);
+        rule.unshift('init');
+
+        let x = 100;
+        let y = 110;
+        let length = 300 / 3**recursionSteps;
+        let angle = 0;
+
+        drawText(context, `${rule.length} segments`, 20, 40);
+
+        drawLineInit(context, x, y);
+
+        rule.forEach(element => {
+
+            switch (element) {
+                case 'L':
+                    angle += Math.PI / 3.0;
+                    break;
+                case 'R':
+                    angle -= 2.0 * Math.PI / 3.0;
+                    break;
+                default:
+                    break;
+            }
+
+            x += Math.cos(angle) * length;
+            y -= Math.sin(angle) * length;
+
+            drawLine(context, x, y);
+        });
     });
     // TODO draw method in depencyList (2nd argument ?!)
 
