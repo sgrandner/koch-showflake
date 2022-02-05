dc082b48dca0a971802591daf40712cea5ea91bd
Stefan Grandner <stefan.grandner@stayfriends.com>
2022-02-05 22:35:04 +0100

    added iterative method, renamed variables

--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -3,6 +3,9 @@ import { useEffect, useRef } from "react";
 
 const Canvas = props => {
 
+    // https://www.section.io/engineering-education/desktop-application-with-react/
+    // https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258
+
     const canvasRef = useRef(null);
 
     const clearCanvas = (ctx) => {
@@ -13,7 +16,7 @@ const Canvas = props => {
 
     const drawText = (ctx, text, x, y) => {
 
-        ctx.font = '20px serif';
+        ctx.font = '16px serif';
         ctx.fillStyle = '#000000';
         ctx.fillText(text, x, y);
     };
@@ -30,26 +33,67 @@ const Canvas = props => {
         ctx.stroke();
     };
 
-    const ruleStep = [ 'L', 'R', 'L' ];
-    const recursionSteps = 3;
+    const elementaryRule = [ 'L', 'R', 'L' ];
+    const stepCount = 3;
+    const calculationType = 'recursive';    // 'recursive' | 'iterative'
 
-    const determineRule = (step, index) => {
+    let recursionCount = 0;
+    let iterationCount = 0;
+    let joinCount = 0;
 
-        if (index > recursionSteps) {
-            return [];
+    const determineRuleRecursive = (rule, stepIndex) => {
+
+        if (stepIndex >= stepCount) {
+            return rule;
         }
 
-        const nextStepPart = determineRule(ruleStep, ++index);
+        recursionCount++;
 
-        let nextStep = [];
-        step.forEach((element) => {
+        const nextRulePart = determineRuleRecursive(elementaryRule, ++stepIndex);
 
-            nextStep.push(...nextStepPart);
-            nextStep.push(element);
+        let nextRule = [];
+        rule.forEach((element) => {
+
+            joinCount++;
+
+            nextRule.push(...nextRulePart);
+            nextRule.push(element);
         });
-        nextStep.push(...nextStepPart);
 
-        return nextStep;
+        joinCount++;
+        nextRule.push(...nextRulePart);
+
+        return nextRule;
+    };
+
+    const determineRuleIterative = (rule) => {
+
+        let currentRule = rule;
+
+        for (let stepIndex = 0; stepIndex < stepCount; stepIndex++) {
+
+            iterationCount++;
+            let joinCountPart = 0
+
+            const nextRule = [];
+
+            currentRule.forEach((element) => {
+
+                joinCountPart++;
+
+                nextRule.push(...elementaryRule);
+                nextRule.push(element);
+            });
+
+            joinCount += joinCountPart;
+
+            joinCount++;
+            nextRule.push(...elementaryRule);
+
+            currentRule = nextRule;
+        }
+
+        return currentRule;
     };
 
     useEffect(() => {
@@ -59,20 +103,34 @@ const Canvas = props => {
 
         clearCanvas(context);
 
-        if (recursionSteps > 5) {
+        if (stepCount > 5) {
             drawText(context, 'too many recursion steps (max. 5) !', 30, 50);
             return;
         }
 
-        const rule = determineRule([ 'R', 'R' ], 0);
-        rule.unshift('init');
+        drawText(context, calculationType, 10, 20);
+
+        let rule;
+        if (calculationType === 'recursive') {
+
+            rule = determineRuleRecursive([ 'R', 'R' ], 0);
+
+        } else if (calculationType === 'iterative') {
+
+            rule = determineRuleIterative([ 'R', 'R' ]);
+        }
+
+        rule.unshift('init');       // first line
 
         let x = 100;
         let y = 110;
-        let length = 300 / 3**recursionSteps;
+        let length = 300 / 3**stepCount;
         let angle = 0;
 
-        drawText(context, `${rule.length} segments`, 20, 40);
+        drawText(context, `${rule.length} segments`, 10, 40);
+        drawText(context, `${recursionCount} recursions`, 10, 60);
+        drawText(context, `${iterationCount} iterations`, 10, 80);
+        drawText(context, `${joinCount} joins`, 10, 100);
 
         drawLineInit(context, x, y);
 
@@ -95,7 +153,6 @@ const Canvas = props => {
             drawLine(context, x, y);
         });
     });
-    // TODO draw method in depencyList (2nd argument ?!)
 
     return <canvas ref={canvasRef} {...props} />;
 };
